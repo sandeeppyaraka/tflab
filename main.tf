e6fcb0048cdb0deed320435b3a1ba380f2bc12c9
Sandeep Pyaraka <sandeep.pyaraka@mitre10.co.nz>
2021-07-23 15:23:12 +1200

    Add Ec2

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ provider "aws" {
     region = "ap-southeast-2"
 }
 
-//resource "aws_instance" "hello sandy" {
-//    ami = ""
-//}
+resource "aws_instance" "hello sandy" {
+    ami = "ami-830c94e3"
+    instance_type = "t2.micro"
+    
+    tags = {
+    Name = "ExampleAppServerInstance"
+}
